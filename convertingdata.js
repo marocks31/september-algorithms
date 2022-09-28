82c0b5248f08b9fa11b8e4bf6431bd1ad0b2e9a3
marocks31 <mollyarisley@gmail.com>
2022-09-28 14:35:02 -0500

    working on converting data types, in the middle of question 4

--- convertingdata.js
+++ convertingdata.js
@@ -1,14 +1,61 @@
 // #  1. Convert an array of arrays into a hash.
 // #     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
 
+// var arrayOfArrays = [
+//   [1, 3],
+//   [8, 9],
+//   [2, 16],
+// ];
+// var hash = {};
+// var index = 0;
+// while (index < arrayOfArrays.length) {
+//   var key = arrayOfArrays[index][0];
+//   var value = arrayOfArrays[index][1];
+//   hash[key] = value;
+//   index++;
+// }
+// console.log(hash);
+
 // #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 // #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+// var arrayOfHashes = [
+//   { id: 1, color: "blue", price: 32 },
+//   { id: 2, color: "red", price: 12 },
+// ];
+// var hash = {};
+// var index = 0;
+// while (index < arrayOfHashes.length) {
+//   hash[arrayOfHashes[index]["id"]] = arrayOfHashes[index];
+//   index++;
+// }
+// console.log(hash);
 
 // #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 // #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
 
+// var originalString = "bookkeeper";
+// var countHash = {};
+// var index = 0;
+// while (index < originalString.length) {
+//   var letter = originalString[index];
+//   if (countHash[letter] === undefined) {
+//     countHash[letter] = 0;
+//   }
+//   countHash[letter] += 1;
+//   index++;
+// }
+// console.log(countHash);
+
 // #  4. Convert a hash into an array of arrays.
 // #     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+var originalHash = { chair: 100, book: 14 };
+var arrayOfArrays = [];
+var index = 0;
+while (index < originalHash.length) {
+  arrayOfArrays = originalHash[index];
+  index++;
+}
+console.log(arrayOfArrays);
 
 // #  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
 // #     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
